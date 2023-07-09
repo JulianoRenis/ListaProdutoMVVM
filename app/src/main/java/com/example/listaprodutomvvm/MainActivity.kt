2d1280ee5b75f7987206155fd7d61cf82ddbc86f
JULIANO RENIS GOMES SILVA DOS SANTOS <julianorenis@gmail.com>
2023-07-09 20:19:00 -0300

    Função add lista adionada. Falta add função remove

--- app/src/main/java/com/example/listaprodutomvvm/MainActivity.kt
+++ app/src/main/java/com/example/listaprodutomvvm/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.listaprodutomvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.listaprodutomvvm.adapter.ItemAdapter
+import com.example.listaprodutomvvm.databinding.ActivityMainBinding
+import com.example.listaprodutomvvm.model.ItemModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var itemAdapter: ItemAdapter
+    private val listItem : MutableList<ItemModel> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val recyclerViewItemProduto = binding.idRv
+        recyclerViewItemProduto.layoutManager = LinearLayoutManager(this)
+        recyclerViewItemProduto.setHasFixedSize(true)
+        itemAdapter = ItemAdapter(this,listItem)
+        recyclerViewItemProduto.adapter = itemAdapter
+
+        binding.button.setOnClickListener {
+
+
+            if (binding.edtTextInserir.text.toString().isEmpty()){
+                binding.edtTextInserir.error = "Preencha um valor"
+                return@setOnClickListener
+            }
+            val item = ItemModel(name = binding.edtTextInserir.text.toString())
+            itemAdapter.addItem(item)
+            binding.edtTextInserir.text!!.clear()
+        }
+
     }
+
 }
--- app/src/main/java/com/example/listaprodutomvvm/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/listaprodutomvvm/adapter/ItemAdapter.kt
@@ -1,23 +1,36 @@
 package com.example.listaprodutomvvm.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.listaprodutomvvm.R
+import com.example.listaprodutomvvm.databinding.ItemProdutoBinding
+import com.example.listaprodutomvvm.model.ItemModel
 
-class ItemAdapter : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
-    class ItemViewHolder (view: View): RecyclerView.ViewHolder(view){
+class ItemAdapter (val context: Context,private val items: MutableList<ItemModel>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
+    class ItemViewHolder(val binding : ItemProdutoBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ItemModel){
+          binding.txtItemAdapter.text = item.name
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        TODO("Not yet implemented")
+        val view = ItemProdutoBinding.inflate(LayoutInflater.from(context),parent, false)
+        return ItemViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = items[position]
+        holder.bind(item)
+    }
+
+    fun addItem(newItem: ItemModel) {
+        items.add(newItem)
+        notifyDataSetChanged()
     }
 }
